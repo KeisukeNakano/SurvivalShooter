22d0612cabfd019ebd695ba126de883e39d77820
keisuke.nakano <keisuke.nakano@aktsk.jp>
2015-04-14 20:30:57 +0900

    add button title scene

---
+++ Assets/Scripts/Button/TestButtonClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void ClickTest () {
+		Debug.Log ("Click");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
